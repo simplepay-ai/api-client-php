7cda4282e68bbc76038eaefa83298d2066cfc68a
Catzilla <11861208+Catzilla@users.noreply.github.com>
2024-12-19 12:44:14 +0200

    Small fix

--- src/Url.php
+++ src/Url.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace SimplePay\ApiClient;
 
+use SimplePay\ApiClient\UrlServices\InvoiceService;
+
 final class Url
 {
+    public InvoiceService $invoice;
+
     public function __construct(
         private string $baseHostname = 'simplepay.ai'
     ) {
+        $this->invoice = new InvoiceService($baseHostname);
     }
 }
