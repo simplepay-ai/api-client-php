f7d481a5e9da057b88f34932e10d79c75cb521e2
Catzilla <11861208+Catzilla@users.noreply.github.com>
2024-12-19 13:00:52 +0200

    Added returnTo

--- src/UrlServices/InvoiceService.php
+++ src/UrlServices/InvoiceService.php
@@ -8,13 +8,25 @@ use SimplePay\ApiClient\Models\Invoice;
 
 final class InvoiceService extends BaseService
 {
-    public function pay(Invoice $invoice): string
+    public function pay(Invoice $invoice, ?string $returnTo = null): string
     {
-        return $this->payById($invoice->id);
+        return $this->payById($invoice->id, $returnTo);
     }
 
-    public function payById(string $invoiceId): string
+    public function payById(string $invoiceId, ?string $returnTo = null): string
     {
-        return sprintf('https://pay.%s/invoice/%s', $this->baseHostname, $invoiceId);
+        $url = sprintf('https://pay.%s/invoice/%s', $this->baseHostname, $invoiceId);
+
+        $query = [];
+
+        if ($returnTo !== null) {
+            $query['return_to'] = $returnTo;
+        }
+
+        if (count($query) > 0) {
+            $url .= '?' . http_build_query($query);
+        }
+
+        return $url;
     }
 }
